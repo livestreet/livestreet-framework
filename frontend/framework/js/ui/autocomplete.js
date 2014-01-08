143860c7eba5a047551a92203d1a12e270c81c27
Mzhelskiy Maxim <rus.engine@gmail.com>
2014-01-08 13:36:09 +0700

    add params to autocompleter

--- frontend/framework/js/ui/autocomplete.js
+++ frontend/framework/js/ui/autocomplete.js
@@ -3,7 +3,7 @@
  * Обертка для jQuery UI виджета autocomplete
  *
  * @module autocomplete
- * 
+ *
  * @license   GNU General Public License, version 2
  * @copyright 2013 OOO "ЛС-СОФТ" {@link http://livestreetcms.com}
  * @author    Denis Shakhov <denis.shakhov@gmail.com>
@@ -15,41 +15,44 @@ ls.autocomplete = (function ($) {
 	/**
 	 * Добавляет автокомплитер к полю ввода
 	 */
-	this.add = function(obj, sPath, multiple) {
+	this.add = function(obj, sPath, multiple, params) {
+		params = params || {};
 		if (multiple) {
 			obj.bind("keydown", function(event) {
 				if ( event.keyCode === $.ui.keyCode.TAB && $( this ).data( "autocomplete" ).menu.active ) {
 					event.preventDefault();
 				}
 			})
-			.autocomplete({
-				source: function(request, response) {
-					ls.ajax.load(sPath, { value: ls.autocomplete.extractLast(request.term) }, function(data){
-						response(data.aItems);
-					});
-				},
-				search: function() {
-					var term = ls.autocomplete.extractLast(this.value);
-					if (term.length < 2) {
+				.autocomplete({
+					source: function(request, response) {
+						params.value = ls.autocomplete.extractLast(request.term);
+						ls.ajax.load(sPath, params, function(data){
+							response(data.aItems);
+						});
+					},
+					search: function() {
+						var term = ls.autocomplete.extractLast(this.value);
+						if (term.length < 2) {
+							return false;
+						}
+					},
+					focus: function() {
+						return false;
+					},
+					select: function(event, ui) {
+						var terms = ls.autocomplete.split(this.value);
+						terms.pop();
+						terms.push(ui.item.value);
+						terms.push("");
+						this.value = terms.join(", ");
 						return false;
 					}
-				},
-				focus: function() {
-					return false;
-				},
-				select: function(event, ui) {
-					var terms = ls.autocomplete.split(this.value);
-					terms.pop();
-					terms.push(ui.item.value);
-					terms.push("");
-					this.value = terms.join(", ");
-					return false;
-				}
-			});
+				});
 		} else {
 			obj.autocomplete({
 				source: function(request, response) {
-					ls.ajax.load(sPath, { value: ls.autocomplete.extractLast(request.term) }, function(data){
+					params.value = ls.autocomplete.extractLast(request.term);
+					ls.ajax.load(sPath, params, function(data){
 						response(data.aItems);
 					});
 				}

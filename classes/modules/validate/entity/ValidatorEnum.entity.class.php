506a43ebd7d6abb73a3fd1b9db8b1fa05b3966af
Mzhelskiy Maxim <rus.engine@gmail.com>
2014-05-23 12:08:10 +0700

    Новый валидатор enum #19

---
+++ classes/modules/validate/entity/ValidatorEnum.entity.class.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * LiveStreet CMS
+ * Copyright © 2013 OOO "ЛС-СОФТ"
+ *
+ * ------------------------------------------------------
+ *
+ * Official site: www.livestreetcms.com
+ * Contact e-mail: office@livestreetcms.com
+ *
+ * GNU General Public License, version 2:
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * ------------------------------------------------------
+ *
+ * @link      http://www.livestreetcms.com
+ * @copyright 2013 OOO "ЛС-СОФТ"
+ * @author    Serge Pustovit (PSNet) <light.feel@gmail.com>
+ *
+ */
+
+/**
+ * Валидатор перечислений
+ *
+ * @package engine.modules.validate
+ * @since 1.0
+ */
+class ModuleValidate_EntityValidatorEnum extends ModuleValidate_EntityValidator {
+	/**
+	 * Допускать или нет пустое значение
+	 *
+	 * @var bool
+	 */
+	public $allowEmpty=true;
+	/**
+	 * Массив разрешенных элементов
+	 *
+	 * @var array
+	 */
+	public $enum=array();
+
+	/**
+	 * Запуск валидации
+	 *
+	 * @param mixed $sValue    			Данные для валидации
+	 * @return bool|string
+	 */
+	public function validate($sValue) {
+		/**
+		 * Проверка типа значения
+		 */
+		if (!is_scalar($sValue)) {
+			return $this->getMessage($this->Lang_Get('validate_enum_invalid',null,false),'msg');
+		}
+		/**
+		 * Разрешение на пустое значение
+		 */
+		if ($this->allowEmpty and $this->isEmpty($sValue)) {
+			return true;
+		}
+		/**
+		 * Проверка на вхождение в перечисление
+		 */
+		if (!in_array($sValue,$this->enum)) {
+			return $this->getMessage($this->Lang_Get('validate_enum_not_allowed',null,false),'msg',array('value'=>htmlspecialchars($sValue)));
+		}
+		/**
+		 * Значение корректно
+		 */
+		return true;
+	}
+}

9e6fb37c8ac93a3897b41252b7690d192e309fbf
Denis Shakhov <denis.shakhov@gmail.com>
2015-02-19 19:59:48 +0700

    Компонент lightbox

---
+++ frontend/components/lightbox/js/lightbox.js
@@ -0,0 +1,26 @@
+/**
+ * Lightbox
+ *
+ * @module ls/lightbox
+ *
+ * @license   GNU General Public License, version 2
+ * @copyright 2013 OOO "ЛС-СОФТ" {@link http://livestreetcms.com}
+ * @author    Denis Shakhov <denis.shakhov@gmail.com>
+ */
+
+$.widget( "livestreet.lsLightbox", {
+    /**
+     * Дефолтные опции
+     */
+    options: {},
+
+    /**
+     * Конструктор
+     *
+     * @constructor
+     * @private
+     */
+    _create: function() {
+        this.element.colorbox( this.options );
+    }
+});

4fd902444f9eb7a8803afed66c687d4d525f6e75
Denis Shakhov <denis.shakhov@gmail.com>
2015-09-03 21:44:32 +0700

    Доработка автокомплита

---
+++ frontend/components/field/js/autocomplete.js
@@ -0,0 +1,52 @@
+/**
+ * Autocomplete
+ *
+ * @module ls/autocomplete
+ *
+ * @license   GNU General Public License, version 2
+ * @copyright 2013 OOO "ЛС-СОФТ" {@link http://livestreetcms.com}
+ * @author    Denis Shakhov <denis.shakhov@gmail.com>
+ */
+
+(function($) {
+    "use strict";
+
+    $.widget( "livestreet.lsAutocomplete", {
+        /**
+         * Дефолтные опции
+         */
+        options: {
+            multiple: false,
+            // Ссылки
+            urls: {
+                load: null
+            },
+            responseName: 'aItems',
+            params: {}
+        },
+
+        /**
+         * Конструктор
+         *
+         * @constructor
+         * @private
+         */
+        _create: function () {
+            this.element.autocomplete({
+                serviceUrl: this.option( 'urls.load' ),
+                type: 'POST',
+                dataType: 'json',
+                paramName: 'value',
+                delimiter: this.option( 'multiple' ) ? ',' : null,
+                transformResult: function(response) {
+                    return {
+                        suggestions: response[ this.option( 'responseName' ) ]
+                    };
+                }.bind(this),
+                params: {
+                    security_ls_key: LIVESTREET_SECURITY_KEY
+                }
+            });
+        }
+    });
+})(jQuery);

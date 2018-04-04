9c7f5eb0751d8a3999f87d1339d2b82aa7bbcd5a
1F6B6 <1F6B6@mail.ru>
2018-04-04 19:27:33 +0500

    Добавление относительной даты для дней/недель/месяцев/лет

--- classes/modules/viewer/plugs/function.date_format.php
+++ classes/modules/viewer/plugs/function.date_format.php
@@ -25,16 +25,20 @@
  * формы слова и поддержкой мультиязычноти.
  *
  * Список ключей параметров:
- *        date*          [string]
- *        format*        [string]
- *        declination*   [int]
- *        now*           [int]    Количество секунд, в течении которых событие имеет статус "Только что"
- *        day*           [string] Указывает на необходимость замены "Сегодня", "Вчера", "Завтра".
- *                                В указанном формате 'day' будет заменено на соответствующее значение.
- *        minutes_back*  [int]    Количество минут, в течении которых событие имеет статус "... минут назад"
- *        hours_back*    [int]    Количество часов, в течении которых событие имеет статус "... часов назад"
- *        tz*               [float]  Временная зона
- *        notz*           [bool]   Не учитывать зону
+ *  date*          [string]
+ *  format*        [string]
+ *  declination*   [int]
+ *  now*           [int]    Количество секунд, в течении которых событие имеет статус "Только что"
+ *  day*           [string] Указывает на необходимость замены "Сегодня", "Вчера", "Завтра". 
+ *                          В указанном формате 'day' будет заменено на соответствующее значение.
+ *  minutes_back*  [int]    Количество минут, в течении которых событие имеет статус "... минут назад"
+ *  hours_back*    [int]    Количество часов, в течении которых событие имеет статус "... часов назад"
+ *  days_back*     [int]    Количество дней, в течении которых событие имеет статус "... дней назад"
+ *  weeks_back*    [int]    Количество недель, в течении которых событие имеет статус "... недель назад"
+ *  months_back*   [int]    Количество месяцев, в течении которых событие имеет статус "... месяцев назад"
+ *  years_back*    [int]    Количество лет, в течении которых событие имеет статус "... лет назад"
+ *  tz*            [float]  Временная зона
+ *  notz*          [bool]   Не учитывать зону
  *
  * (* - параметр является необязательным)
  *
@@ -49,7 +53,7 @@ function smarty_function_date_format($aParams, &$oSmarty)
     $sResult = $oEngine->Viewer_GetDateFormat(
         isset($aParams['date']) ? $aParams['date'] : null,
         isset($aParams['format']) ? $aParams['format'] : 'd F Y, H:i',
-        array_intersect_key($aParams, array_flip(array('declination', 'now', 'day', 'minutes_back', 'hours_back', 'tz', 'notz')))
+        array_intersect_key($aParams, array_flip(array('declination', 'now', 'day', 'minutes_back', 'hours_back', 'days_back', 'weeks_back', 'months_back', 'years_back', 'tz', 'notz')))
     );
     if (!empty($aParams['assign'])) {
         $oSmarty->assign($aParams['assign'], $sResult);
